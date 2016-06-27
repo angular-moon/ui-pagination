4ce9a56e63ecece3b64c02cb75a66e3534b2b2df
gmsoft <admin@example.com>
2016-06-27 10:57:17 +0800

    修改跳转页input不为必填

--- ui-pagination.js
+++ ui-pagination.js
@@ -30,8 +30,8 @@ angular.module("ui.pagination", ['ui.pagination.tpls'])
             '<div class="pagination info">共 <span style="color:#ac2925">{{totalItems}}</span> 条 ' +
             '[每页 <span style="color:#808080">{{itemsPerPage}}</span> 条]</div>' +
             '<div class="pagination jump" ng-if="numPages> 5">' +
-            '共 {{numPages}} 页 去第<form name="jumpForm" style="display: inline"><input type="text" name="jumpInput" ng-model="jumpInput" required ng-pattern="/^[0-9]*$/"></form>页' +
-            '<button class="btn btn-default" ng-disabled="jumpForm.$invalid" ng-click="__setPage(jumpInput<1?(jumpInput=1):jumpInput>numPages?(jumpInput=numPages):jumpInput)">跳转</button></div>' +
+            '共 {{numPages}} 页 去第<form name="jumpForm" style="display: inline"><input type="text" name="jumpInput" ng-model="jumpInput" ng-pattern="/^[0-9]*$/"></form>页' +
+            '<button class="btn btn-default" ng-disabled="!jumpInput || jumpForm.$invalid" ng-click="__setPage(jumpInput<1?(jumpInput=1):jumpInput>numPages?(jumpInput=numPages):jumpInput)">跳转</button></div>' +
             '<div style="float:right"><div origin-pagination total-items="totalItems" ng-model="currentPage" items-per-page="itemsPerPage" max-size="5" rotate="false" num-pages="numPages"></div></div>' +
             '<i style="clear:both"></i></div>',
         link: function(scope, element, attrs){
